4c1114fd16b056870c24a62caf7fb774b2220186
JAYESHBHUT <SHREYINNOVATIONS@GMAIL.COM>
2018-02-27 01:52:34 +0530

    Add files via upload

---
+++ audioAssets.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Audio Source - JUST FOR TESTING PURPOSE : https://s3.amazonaws.com/simusicbucket
+var audioData = [
+	{
+        'title' : 'Memory of Kinnari',
+        'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/01+Kinnari.mp3'
+    },
+	{
+        'title' : 'Ek Radha Ek Meera',
+		'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/02+Ek+Radha+Ek+Meera.mp3'
+    },
+    {
+        'title' : 'Kadam Kadam Par',
+        'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/03+Kadam+Kadam+Par.mp3'
+    },
+    {
+        'title' : 'Om Sai Namoh Namah',
+        'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/05+Om+Sai+Namo+Namaha.mp3'
+    },
+    {
+        'title' : 'Om Gan Ganapataye Namoh Namah',
+        'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/04+Om+Gann+Ganapataye.mp3'
+    },
+{
+        'title' : 'This is the end of playlist',
+        'url' : 'https://s3.amazonaws.com/simusicbucket/vahetu-jivan/01+Kinnari.mp3'
+    }
+];
+
+module.exports = audioData;
